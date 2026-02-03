8dadb050fe9307d50b6b5d43e9c662baff1d17ca
RAJ KUMAR NISHAD <rajkumarnishad96930@gmail.com>
2026-02-03 18:45:35 +0530

    update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
-
+import './practice'
 
 
 // const App = () => {
---
+++ practice.js
@@ -0,0 +1,15 @@
+// practice.js
+
+const add = (a,b) => a+b
+console.log(add(2,3))
+
+const user = {
+  name: "Raj",
+  age: 21
+}
+
+console.log(user.name)
+
+const arr = [1,2,3,4]
+const doubled = arr.map(x => x*2)
+console.log(doubled)
